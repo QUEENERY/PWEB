c18098709bad51e0e8afa535fdd11497fcd0146f
QUEENERY <114456736+QUEENERY@users.noreply.github.com>
2023-03-16 21:24:52 +0800

    Add files via upload

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/apiProduk', [ProductController::class, 'index']);
+Route::get('/detailProduk/{id}', [ProductController::class, 'show']);
+Route::post('/createProduk', [ProductController::class, 'store']);
+Route::put('/updateProduk/{id}', [ProductController::class, 'update']);
+Route::delete('/deleteProduk/{id}', [ProductController::class, 'destroy']);
+
+Route::get('/dataUser', [UserController::class, 'index']);
+Route::get('/totalUser', [UserController::class, 'total_user']);
